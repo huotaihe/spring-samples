4867918fb83d19bf956bc337cb4cf5d2fe2f1886
寒溪 <taihe.hth@alibaba-inc.com>
2021-05-28 19:13:08 +0800

    循环依赖案例

---
+++ spring-boot/circular-reference/src/main/java/com/huotaihe/sample/spring/boot/A.java
@@ -0,0 +1,21 @@
+package com.huotaihe.sample.spring.boot;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 寒溪
+ * @date 2021/05/28
+ */
+@Component
+public class A {
+
+    public A(B b) {
+        c
+    }
+
+    @Resource
+    private B b;
+
+}
---
+++ spring-boot/circular-reference/src/main/java/com/huotaihe/sample/spring/boot/B.java
@@ -0,0 +1,19 @@
+package com.huotaihe.sample.spring.boot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 寒溪
+ * @date 2021/05/28
+ */
+@Component
+public class B {
+
+    public B(A a) {
+
+    }
+
+    @Autowired
+    private A a;
+}
