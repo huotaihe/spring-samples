59d9218f6de8abebcdebc8a78e02d7cef2fe76f0
寒溪 <taihe.hth@alibaba-inc.com>
2021-06-03 09:33:55 +0800

    JSON 序列化反序列化案例

--- spring-boot/json-serialize-deserialize/src/main/java/com/huotaihe/sample/spring/boot/MemberLevelController.java
+++ spring-boot/json-serialize-deserialize/src/main/java/com/huotaihe/sample/spring/boot/MemberLevelController.java
@@ -1,7 +1,7 @@
 package com.huotaihe.sample.spring.boot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,6 +17,12 @@ public class MemberLevelController {
     }
 
     @GetMapping("/member/level2")
+    public MemberLevelEnum setLevel(@RequestBody RequestVO request) {
+
+        return request.getLevel();
+    }
+
+    @GetMapping("/member/level3")
     public MemberLevelEnum setLevel(MemberLevelEnum level) {
 
         return level;
---
+++ spring-boot/json-serialize-deserialize/src/main/java/com/huotaihe/sample/spring/boot/RequestVO.java
@@ -0,0 +1,14 @@
+package com.huotaihe.sample.spring.boot;
+
+import lombok.Data;
+
+/**
+ * @author 寒溪
+ * @date 2021/06/02
+ */
+@Data
+public class RequestVO {
+
+    private MemberLevelEnum level;
+
+}
